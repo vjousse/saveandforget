7ee7776ff207a018d1a037a99a99c3a14040dc5c
Vincent Jousse <vincent@jousse.org>
2020-03-28 21:24:19 +0100

    Add futures for download

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,11 +2,16 @@ use std::collections::HashMap;
 
 pub type Url = String;
 
-pub async fn download_files(_urls: &Vec<Url>) -> Result<i32, Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+pub async fn download_files(urls: &Vec<Url>) -> Result<i32, Box<dyn std::error::Error>> {
+
+    // https://users.rust-lang.org/t/solved-problem-with-reqwest-0-10-async-client/36373/13
+    // http://patshaughnessy.net/2020/1/20/downloading-100000-files-using-async-rust
+    for url in urls.iter() {
+        let resp = reqwest::get("https://httpbin.org/ip")
+            .await?
+            .json::<HashMap<String, String>>()
+            .await?;
+        println!("{:#?}", resp);
+    }
     Ok(1)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate saveandforget as saf;
+use futures::executor::block_on;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let test_event = saf::messenger::get_full_test_event();
     match saf::messenger::parse_document(test_event) {
-        Ok(urls) => saf::core::download_files(&urls),
-        Err(_) => (),
+        Ok(urls) => block_on(saf::core::download_files(&urls)),
+        Err(_) => Ok(0),
     };
 
     println!("Hello, world!");
