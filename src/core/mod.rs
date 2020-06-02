c54e8e1bcc8d9a50cb0e61bc389685fa692e35a6
Vincent Jousse <vincent@jousse.org>
2020-06-02 22:46:02 +0200

    Join futures to download files

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,6 @@
+use futures::future::join_all;
+use futures::stream::{self, StreamExt};
+use futures::future::Future;
 use reqwest::Client;
 use std::path::Path;
 use tokio::prelude::*;
@@ -6,8 +9,56 @@ use uuid::Uuid;
 
 pub type Url = String;
 
-pub async fn add_new_file(_file: &Path) -> () {
+pub async fn download_files_join(urls: &Vec<Url>, destination_dir: &Path) -> Vec<Result<String, Box<dyn std::error::Error>>> {
 
+    let client = Client::new();
+
+    let downloading_futures =
+        urls.iter()
+            .map(|url| download_file(&client, url, destination_dir));
+
+    join_all(downloading_futures).await
+}
+
+pub async fn download_file(client: &Client, url: &Url, destination_dir: &Path) -> Result<String, Box<dyn std::error::Error>> {
+
+    match client.get(url).send().await {
+
+        Ok(mut resp) => {
+            dbg!(format!("Downloaded {}", &url));
+            let content_type:Option<&str> = extract_header(resp.headers().get("content-type"));
+            dbg!(content_type);
+
+            let my_uuid = Uuid::new_v4();
+            let destination_file = destination_dir.join(my_uuid.to_string() +
+            match content_type {
+                Some("image/png") => ".png",
+                Some("image/jpeg") => ".jpg",
+                Some("image/gif") => ".gif",
+                Some(_) => "",
+                None => ""
+            });
+
+            dbg!(&destination_file);
+
+            match File::create(&destination_file).await {
+                Ok(mut file) => {
+                    while let Some(chunk) = resp.chunk().await? {
+                        file.write_all(&chunk).await?;
+                    };
+                    Ok(destination_file.to_str().unwrap_or("").to_owned())
+                },
+                Err(err) => Err(Box::new(err))
+            }
+
+        }
+        Err(err) => {
+            println!("Error downloading {:#?}", err);
+            Err(Box::new(err))
+        }
+    }
+
+    //Ok("test".to_owned())
 }
 
 pub async fn download_files(urls: &Vec<Url>, destination_dir: &Path) -> Result<i32, Box<dyn std::error::Error>> {
