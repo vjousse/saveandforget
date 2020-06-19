03eaa15194cf6df8ff1e23455440fed5320cd257
Vincent Jousse <vincent@jousse.org>
2020-06-19 07:02:58 +0200

    Remove useless comments

--- src/main.rs
+++ src/main.rs
@@ -33,49 +33,6 @@ mod config {
     }
 }
 
-/*
-#[tokio::main]
-async fn main() {
-    dotenv().expect("Failed to read .env file");
-    let test_event = saf::messenger::get_full_test_event();
-    let config = crate::config::Config::from_env().unwrap();
-
-    let pg_connection_pool:PgPooledConnection =
-        db_connection::get_connection_pool(config.database_url)
-            .get()
-            .expect("Impossible to connect to DATABASE");
-
-    let path = Path::new(&config.download_path);
-
-    let urls = match saf::messenger::parse_document(test_event) {
-        Ok(urls) => {
-            let files:Vec<Result<String,Box<dyn std::error::Error>>> =
-                saf::core::download_files(&urls, path).await;
-            files
-        },
-        Err(_) => vec![]
-    };
-
-    let documents = 
-        urls
-            .iter()
-            .map(
-            // Map over the Vec of destination files
-                |result| result.as_ref().map(
-                    // Convert Result<String,_> into Result<Document,_>
-                    |download_path| NewDocument::new(download_path.to_owned(), Some("Test description".to_owned()))
-                ).map(|new_doc| new_doc.create(&pg_connection_pool))
-            )
-            .collect::<Vec<_>>();
-
-
-
-
-    dbg!(documents);
-}
-*/
-
-
 
 async fn index(_req: HttpRequest, pg_pool: web::Data<PgPool>) -> Result<HttpResponse, Error> {
     let documents= 
