a797412568312faa5e7753bf642852a3ccb5b7f7
Vincent Jousse <vincent@jousse.org>
2020-06-24 11:04:32 +0200

    Load first user

--- src/bin/load_fixtures.rs
+++ src/bin/load_fixtures.rs
@@ -1,10 +1,13 @@
 extern crate dotenv;
 extern crate saveandforget as saf;
+#[macro_use]
+extern crate log;
 
 use dotenv::dotenv;
 
 use saf::db;
 use saf::db::PgPool;
+use saf::models::user::{RegisterUser, User};
 
 mod config {
     pub use config::ConfigError;
@@ -24,7 +27,17 @@ mod config {
 
 fn main() {
     dotenv().ok();
+    env_logger::init();
+    debug!("Loading fixtures");
 
     let config = crate::config::Config::from_env().expect("Error reading .env file.");
     let pg_pool: PgPool = db::init_pool(&config.database_url).expect("Failed to create pool");
+
+    let first_user = RegisterUser {
+        email: "vin.cent@hey.com".to_owned(),
+        password: "test".to_owned(),
+        password_confirmation: "test".to_owned(),
+    };
+    let user_created = User::create(first_user, &pg_pool);
+    debug!("{:?}", user_created);
 }
